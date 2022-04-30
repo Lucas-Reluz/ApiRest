0b7061dd825500283f01133db11ab2283d1bc9a7
Lucas-Reluz <lucas.pokemon.61@gmail.com>
2022-04-30 18:23:29 -0300

    @Lucas-Reluz [Commit 3: Criando Domain com Cliente] {30/04/2022 18:22}

---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid ID { get; set; }
+        [Required(ErrorMessage = "O campo {0} nome é obrigatório")]
+        [MaxLength(70, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres")]
+        public string Name { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+    }
+}
